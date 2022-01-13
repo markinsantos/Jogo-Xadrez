4f144c917ea66af674f620b037c7620dd8995554
markinsantos <marcosdosantosilva@gmail.com>
2022-01-13 12:35:43 -0300

    implementando jogada especial en passan

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -21,6 +21,7 @@ public class PartidaXadrez {
 	private Cor jogadorAtual;
 	private boolean check;
 	private boolean checkMate;
+	private PecaXadrez enPassan;
 
 	List<Peca> pecasNoTabuleiro = new ArrayList<Peca>();
 	List<Peca> pecasCapturadas = new ArrayList<Peca>();
@@ -45,10 +46,15 @@ public class PartidaXadrez {
 	public boolean getCheck() {
 		return check;
 	}
+
 	public boolean getCheckMate() {
 		return checkMate;
-		
-		}
+
+	}
+
+	public PecaXadrez getEnPassan() {
+		return enPassan;
+	}
 
 	public PecaXadrez[][] getPecas() {
 		PecaXadrez[][] mat = new PecaXadrez[board.getLinhas()][board.getColunas()];
@@ -72,25 +78,37 @@ public class PartidaXadrez {
 		validarPosicaoInicial(inicial);
 		validarPosicaoDestino(inicial, target);
 		Peca pecaCapturada = moverPeca(inicial, target);
+
 		if (TesteCheck(jogadorAtual)) {
 			desfazerMovimento(inicial, target, pecaCapturada);
 			throw new ExecaoXadrez("Não é permitido se colocar em check");
 		}
+
+		PecaXadrez pecaMovida = (PecaXadrez) board.peca(target);
 		check = (TesteCheck(oponente(jogadorAtual))) ? true : false;
-		
-		if(TesteCheckMate(oponente(jogadorAtual))) {
+
+		if (TesteCheckMate(oponente(jogadorAtual))) {
 			checkMate = true;
-			
-		}else {
-		
-		proximoTurno();
+
+		} else {
+
+			proximoTurno();
 		}
+
+		// movimento especial en passan
+		if (pecaMovida instanceof Peao
+				&& (target.getLinha() == inicial.getLinha() - 2 || target.getLinha() == inicial.getLinha() + 2)) {
+			enPassan = pecaMovida;
+		} else {
+			enPassan = null;
+		}
+
 		return (PecaXadrez) pecaCapturada;
 
 	}
 
 	private Peca moverPeca(Posicao inicial, Posicao target) {
-		PecaXadrez peca = (PecaXadrez)board.removePeca(inicial);
+		PecaXadrez peca = (PecaXadrez) board.removePeca(inicial);
 		peca.incrementarMovimentos();
 		Peca pecaCapturada = board.removePeca(target);
 		board.PosicaoPeca(peca, target);
@@ -101,22 +119,41 @@ public class PartidaXadrez {
 
 		}
 		// movimento especial rook
-		if(peca instanceof Rei && target.getColuna()== inicial.getColuna()+2) {
-			Posicao inicialTorre = new Posicao(inicial.getLinha(),inicial.getColuna()+3);
-			Posicao targetTorre = new Posicao(inicial.getLinha(),inicial.getColuna()+1);
+		if (peca instanceof Rei && target.getColuna() == inicial.getColuna() + 2) {
+			Posicao inicialTorre = new Posicao(inicial.getLinha(), inicial.getColuna() + 3);
+			Posicao targetTorre = new Posicao(inicial.getLinha(), inicial.getColuna() + 1);
 			PecaXadrez rook = (PecaXadrez) board.removePeca(inicialTorre);
 			board.PosicaoPeca(rook, targetTorre);
 			rook.incrementarMovimentos();
 		}
-		
-		if(peca instanceof Rei && target.getColuna()== inicial.getColuna()-2) {
-			Posicao inicialTorre = new Posicao(inicial.getLinha(),inicial.getColuna()-4);
-			Posicao targetTorre = new Posicao(inicial.getLinha(),inicial.getColuna()-1);
+
+		if (peca instanceof Rei && target.getColuna() == inicial.getColuna() - 2) {
+			Posicao inicialTorre = new Posicao(inicial.getLinha(), inicial.getColuna() - 4);
+			Posicao targetTorre = new Posicao(inicial.getLinha(), inicial.getColuna() - 1);
 			PecaXadrez rook = (PecaXadrez) board.removePeca(inicialTorre);
 			board.PosicaoPeca(rook, targetTorre);
 			rook.incrementarMovimentos();
 		}
-		
+
+		// movimento especial en passan
+
+		if (peca instanceof Peao) {
+
+			if (inicial.getColuna() != target.getColuna() && pecaCapturada == null) {
+				Posicao peaoPosicao;
+				if (peca.getCor() == Cor.BRANCO) {
+					peaoPosicao = new Posicao(target.getLinha() + 1, target.getColuna());
+				} else {
+					peaoPosicao = new Posicao(target.getLinha() - 1, target.getColuna());
+				}
+
+				pecaCapturada = board.removePeca(peaoPosicao);
+				pecasCapturadas.add(pecaCapturada);
+				pecasNoTabuleiro.remove(pecaCapturada);
+
+			}
+
+		}
 
 		return pecaCapturada;
 
@@ -124,7 +161,7 @@ public class PartidaXadrez {
 
 	private void desfazerMovimento(Posicao inicial, Posicao target, Peca pecaCapturada) {
 
-		PecaXadrez p = (PecaXadrez)board.removePeca(target);
+		PecaXadrez p = (PecaXadrez) board.removePeca(target);
 		p.decrementarMovimentos();
 		board.PosicaoPeca(p, inicial);
 		if (pecaCapturada != null) {
@@ -132,23 +169,39 @@ public class PartidaXadrez {
 			pecasCapturadas.remove(pecaCapturada);
 			pecasNoTabuleiro.add(pecaCapturada);
 		}
-		
-		if(p instanceof Rei && target.getColuna()== inicial.getColuna()+2) {
-			Posicao inicialTorre = new Posicao(inicial.getLinha(),inicial.getColuna()+3);
-			Posicao targetTorre = new Posicao(inicial.getLinha(),inicial.getColuna()+1);
+
+		if (p instanceof Rei && target.getColuna() == inicial.getColuna() + 2) {
+			Posicao inicialTorre = new Posicao(inicial.getLinha(), inicial.getColuna() + 3);
+			Posicao targetTorre = new Posicao(inicial.getLinha(), inicial.getColuna() + 1);
 			PecaXadrez rook = (PecaXadrez) board.removePeca(targetTorre);
 			board.PosicaoPeca(rook, inicialTorre);
 			rook.decrementarMovimentos();
 		}
-		
-		if(p instanceof Rei && target.getColuna()== inicial.getColuna()-2) {
-			Posicao inicialTorre = new Posicao(inicial.getLinha(),inicial.getColuna()-4);
-			Posicao targetTorre = new Posicao(inicial.getLinha(),inicial.getColuna()-1);
+
+		if (p instanceof Rei && target.getColuna() == inicial.getColuna() - 2) {
+			Posicao inicialTorre = new Posicao(inicial.getLinha(), inicial.getColuna() - 4);
+			Posicao targetTorre = new Posicao(inicial.getLinha(), inicial.getColuna() - 1);
 			PecaXadrez rook = (PecaXadrez) board.removePeca(targetTorre);
 			board.PosicaoPeca(rook, inicialTorre);
 			rook.decrementarMovimentos();
 		}
-		
+
+		// movemento especial de en passan
+
+		if (p instanceof Peao) {
+
+			if (inicial.getColuna() != target.getColuna() && pecaCapturada == enPassan) {
+				PecaXadrez peao = (PecaXadrez)board.removePeca(target);
+				Posicao peaoPosicao;
+				if (p.getCor() == Cor.BRANCO) {
+					peaoPosicao = new Posicao(3, target.getColuna());
+				} else {
+					peaoPosicao = new Posicao(4, target.getColuna());
+				}
+				board.PosicaoPeca(peao, peaoPosicao);
+				
+			}
+		}
 
 	}
 
@@ -206,32 +259,32 @@ public class PartidaXadrez {
 		}
 		return false;
 	}
-	
+
 	private boolean TesteCheckMate(Cor cor) {
 		;
-		if(!TesteCheck(cor)) {
+		if (!TesteCheck(cor)) {
 			return false;
 		}
-		
-		List<Peca> lista = pecasNoTabuleiro.stream().filter(x -> ((PecaXadrez) x).getCor() == cor).collect(Collectors.toList());
-		
-		for(Peca p: lista) {
+
+		List<Peca> lista = pecasNoTabuleiro.stream().filter(x -> ((PecaXadrez) x).getCor() == cor)
+				.collect(Collectors.toList());
+
+		for (Peca p : lista) {
 			boolean[][] matriz = p.movimentosPossiveis();
-			for(int i=0;i<board.getLinhas();i++) {
-				for(int j=0;j<board.getColunas();j++) {
-					if(matriz[i][j]) {
-						
-						Posicao inicial= ((PecaXadrez)p).getPosicaoXadrez().toPosicao();
-						Posicao target = new Posicao(i,j);
-						Peca pecaCapturada = moverPeca(inicial,target);
+			for (int i = 0; i < board.getLinhas(); i++) {
+				for (int j = 0; j < board.getColunas(); j++) {
+					if (matriz[i][j]) {
+
+						Posicao inicial = ((PecaXadrez) p).getPosicaoXadrez().toPosicao();
+						Posicao target = new Posicao(i, j);
+						Peca pecaCapturada = moverPeca(inicial, target);
 						boolean testeCheck = TesteCheck(cor);
 						desfazerMovimento(inicial, target, pecaCapturada);
-						if(!testeCheck) {
+						if (!testeCheck) {
 							return false;
 						}
 					}
-					
-					
+
 				}
 			}
 		}
@@ -253,15 +306,15 @@ public class PartidaXadrez {
 		posicaoNovaPeca('c', 1, new Bispo(board, Cor.BRANCO));
 		posicaoNovaPeca('f', 1, new Bispo(board, Cor.BRANCO));
 		posicaoNovaPeca('d', 1, new Rainha(board, Cor.BRANCO));
-		posicaoNovaPeca('e', 1, new Rei(board, Cor.BRANCO,this));
-		posicaoNovaPeca('a', 2, new Peao(board, Cor.BRANCO));
-		posicaoNovaPeca('b', 2, new Peao(board, Cor.BRANCO));
-		posicaoNovaPeca('c', 2, new Peao(board, Cor.BRANCO));
-		posicaoNovaPeca('d', 2, new Peao(board, Cor.BRANCO));
-		posicaoNovaPeca('e', 2, new Peao(board, Cor.BRANCO));
-		posicaoNovaPeca('f', 2, new Peao(board, Cor.BRANCO));
-		posicaoNovaPeca('g', 2, new Peao(board, Cor.BRANCO));
-		posicaoNovaPeca('h', 2, new Peao(board, Cor.BRANCO));
+		posicaoNovaPeca('e', 1, new Rei(board, Cor.BRANCO, this));
+		posicaoNovaPeca('a', 2, new Peao(board, Cor.BRANCO, this));
+		posicaoNovaPeca('b', 2, new Peao(board, Cor.BRANCO, this));
+		posicaoNovaPeca('c', 2, new Peao(board, Cor.BRANCO, this));
+		posicaoNovaPeca('d', 2, new Peao(board, Cor.BRANCO, this));
+		posicaoNovaPeca('e', 2, new Peao(board, Cor.BRANCO, this));
+		posicaoNovaPeca('f', 2, new Peao(board, Cor.BRANCO, this));
+		posicaoNovaPeca('g', 2, new Peao(board, Cor.BRANCO, this));
+		posicaoNovaPeca('h', 2, new Peao(board, Cor.BRANCO, this));
 
 		posicaoNovaPeca('a', 8, new Torre(board, Cor.PRETO));
 		posicaoNovaPeca('h', 8, new Torre(board, Cor.PRETO));
@@ -270,16 +323,15 @@ public class PartidaXadrez {
 		posicaoNovaPeca('c', 8, new Bispo(board, Cor.PRETO));
 		posicaoNovaPeca('f', 8, new Bispo(board, Cor.PRETO));
 		posicaoNovaPeca('d', 8, new Rainha(board, Cor.PRETO));
-		posicaoNovaPeca('e', 8, new Rei(board, Cor.PRETO,this));
-		posicaoNovaPeca('a', 7, new Peao(board, Cor.PRETO));
-		posicaoNovaPeca('b', 7, new Peao(board, Cor.PRETO));
-		posicaoNovaPeca('c', 7, new Peao(board, Cor.PRETO));
-		posicaoNovaPeca('d', 7, new Peao(board, Cor.PRETO));
-		posicaoNovaPeca('e', 7, new Peao(board, Cor.PRETO));
-		posicaoNovaPeca('f', 7, new Peao(board, Cor.PRETO));
-		posicaoNovaPeca('g', 7, new Peao(board, Cor.PRETO));
-		posicaoNovaPeca('h', 7, new Peao(board, Cor.PRETO));
-
+		posicaoNovaPeca('e', 8, new Rei(board, Cor.PRETO, this));
+		posicaoNovaPeca('a', 7, new Peao(board, Cor.PRETO, this));
+		posicaoNovaPeca('b', 7, new Peao(board, Cor.PRETO, this));
+		posicaoNovaPeca('c', 7, new Peao(board, Cor.PRETO, this));
+		posicaoNovaPeca('d', 7, new Peao(board, Cor.PRETO, this));
+		posicaoNovaPeca('e', 7, new Peao(board, Cor.PRETO, this));
+		posicaoNovaPeca('f', 7, new Peao(board, Cor.PRETO, this));
+		posicaoNovaPeca('g', 7, new Peao(board, Cor.PRETO, this));
+		posicaoNovaPeca('h', 7, new Peao(board, Cor.PRETO, this));
 
 	}
 }
--- src/xadrez/pecas/Peao.java
+++ src/xadrez/pecas/Peao.java
@@ -3,12 +3,16 @@ package xadrez.pecas;
 import Tabuleiro.Board;
 import Tabuleiro.Posicao;
 import xadrez.Cor;
+import xadrez.PartidaXadrez;
 import xadrez.PecaXadrez;
 
 public class Peao extends PecaXadrez{
+	
+	private PartidaXadrez partida;
 
-	public Peao(Board bord, Cor cor) {
+	public Peao(Board bord, Cor cor,PartidaXadrez partida) {
 		super(bord, cor);
+		this.partida = partida;
 		
 	}
 
@@ -36,6 +40,22 @@ public class Peao extends PecaXadrez{
 				matriz[p.getLinha()][p.getColuna()] = true;
 			}
 			
+			// movimento especial en passan
+			
+			if(posicao.getLinha() == 3) {
+				
+				Posicao left = new Posicao(posicao.getLinha(), posicao.getColuna()-1);
+				if(getBord().existePosicao(left) && existePecaAdversaria(left)&&  getBord().peca(left) == partida.getEnPassan()){
+					matriz[left.getLinha()-1][left.getColuna()] = true;
+				}
+				
+				Posicao right = new Posicao(posicao.getLinha(), posicao.getColuna()+1);
+				if(getBord().existePosicao(right) && existePecaAdversaria(right)&&  getBord().peca(right) == partida.getEnPassan()){
+					matriz[right.getLinha()-1][right.getColuna()] = true;
+				}
+				
+			}
+			
 						
 		}else {
 			p.setValues(posicao.getLinha()+1, posicao.getColuna());
@@ -55,6 +75,22 @@ public class Peao extends PecaXadrez{
 			if(getBord().existePosicao(p)&& existePecaAdversaria(p)) {
 				matriz[p.getLinha()][p.getColuna()] = true;
 			}
+		}
+		
+		// movimentos especial en passan 
+		
+		if(posicao.getLinha() == 4) {
+			
+			Posicao left = new Posicao(posicao.getLinha(), posicao.getColuna()-1);
+			if(getBord().existePosicao(left) && existePecaAdversaria(left)&&  getBord().peca(left) == partida.getEnPassan()){
+				matriz[left.getLinha()+1][left.getColuna()] = true;
+			}
+			
+			Posicao right = new Posicao(posicao.getLinha(), posicao.getColuna()+1);
+			if(getBord().existePosicao(right) && existePecaAdversaria(right)&&  getBord().peca(right) == partida.getEnPassan()){
+				matriz[right.getLinha()+1][right.getColuna()] = true;
+			}
+			
 		}
 			
 		return matriz;
